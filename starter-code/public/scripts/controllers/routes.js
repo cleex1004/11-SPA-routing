c524127d83c9fb7e6bc1ddfe5ab58214c80b7d39
EVE DENISON <evedenison@gmail.com>
2017-02-20 18:55:24 -0800

    configured routes, articleController.js, index.html refactored href, scripts tag for page.js, deleted articleView.js handleMainNav

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,10 +1,9 @@
 'use strict';
 
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
+// DONE: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
-page('/', aboutController.init);
-//page('/new', newView.init);
-//page('/admin', adminView.init);
+page('/', articleController.init);
+page('/about', aboutController.init);
 
 // DONE: What function do you call to activate page.js? Fire it off now, to execute
 page();
